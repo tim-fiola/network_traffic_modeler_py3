344d7a83c05f6af60118323cbdc371c484f22a6f
tim fiola <timothy.fiola@gamil.com>
2019-07-18 16:25:01 -0600

    added rsvp tests

--- test/test_rsvplsp.py
+++ test/test_rsvplsp.py
@@ -2,6 +2,8 @@ import unittest
 
 from pyNTM import Node
 from pyNTM import RSVP_LSP
+from pyNTM import Model
+
 
 
 class TestRSVPLSP(unittest.TestCase):
@@ -9,6 +11,22 @@ class TestRSVPLSP(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.maxDiff = None
-        self.node_a = Node(name='nodeA', lat=0, lon=0)
-        self.node_b = Node(name='nodeB', lat=0, lon=0)
-        self.rsvp_lsp = RSVP_LSP(source_node_object=self.node_a, dest_node_object=self.node_b, lsp_name='A-to-B')
+        self.model = Model.load_model_file('model_test_topology.csv')
+        self.lsp_a_d_1 = self.model.get_rsvp_lsp('A', 'D', 'lsp_a_d_1')
+        self.lsp_a_d_1 = self.model.get_rsvp_lsp('A', 'D', 'lsp_a_d_2')
+        self.model.update_simulation()
+        # self.node_a = Node(name='nodeA', lat=0, lon=0)
+        # self.node_b = Node(name='nodeB', lat=0, lon=0)
+        # self.rsvp_lsp = RSVP_LSP(source_node_object=self.node_a, dest_node_object=self.node_b, lsp_name='A-to-B')
+
+    def test_lsp_instance(self):
+        self.assertIsInstance(self.lsp_a_d_1, RSVP_LSP)
+
+    def test_lsp_path_instance(self):
+        self.assertIsInstance(self.lsp_a_d_1.path, dict)
+
+    def test_lsp_path(self):
+        self.assertEqual(60, self.lsp_a_d_1.path['path_cost'])
+
+    def test_lsp_repr(self):
+        self.assertEqual(repr(self.lsp_a_d_1), "RSVP_LSP(source = A, dest = D, lsp_name = 'lsp_a_d_1')")
