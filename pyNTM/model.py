1632fcc81f4761afc23276c92d30d5da33869f24
tim fiola <timothy.fiola@gamil.com>
2019-08-04 23:03:57 -0600

    test

--- pyNTM/model.py
+++ pyNTM/model.py
@@ -95,7 +95,6 @@ class Model(object):
             self.interface_objects.union(new_interface_objects)
         self.validate_model()
 
-    # TODO - simplify this; it's too complex
     def validate_model(self):
         """
         Validates that data fed into the model creates a valid network model
