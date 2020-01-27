1e74521c275466cfae2b88f9967eec5e40eeaaf5
tim fiola <timothy.fiola@gamil.com>
2020-01-27 16:50:11 -0700

    working thru bugs in prototyping multidigraph

--- examples/multidigraph_prototyping.py
+++ examples/multidigraph_prototyping.py
@@ -30,38 +30,37 @@ def normalize_multiple_links(path_info):
 
         # Each 'hop' is a list of Interfaces from one adjacent node to the
         # next; there may be multiple Interfaces in that list
-        for i in range(max_hop):
-            print("i is {}".format(i))
-            hop = path[i]
+        for hop_number in range(max_hop):
+            print("hop_number is {}".format(hop_number))
+            hop = path[hop_number]
             print("hop is {}".format(hop))
-
 
             if len(hop) == 1:
                 # There is only one Interface in the hop list; use that interface
-                # Add that single interface to hop i in each sub_path in sub_path_list
-                for j in range(len(sub_path_list)):
-                    sub_path_list[j].append(hop)
+                # Add that single interface to hop hop_number in each sub_path in sub_path_list
+                for sub_path_number in range(len(sub_path_list)):
+                    sub_path_list[sub_path_number].append(hop)
             elif len(hop) > 1:
                 # Examine interface cost
                 min_cost = min([interface.cost for interface in hop])
                 hop = [interface for interface in hop if interface.cost == min_cost]
                 if len(hop) == 1:
                     # There is only one Interface in the hop list; use that interface
-                    # Add that single interface to hop i in each sub_path in sub_path_list
-                    for j in range(len(sub_path_list)):
-                        sub_path_list[j].append(hop[0])
+                    # Add that single interface to hop hop_number in each sub_path in sub_path_list
+                    for sub_path_number in range(len(sub_path_list)):
+                        sub_path_list[sub_path_number].append(hop[0])
                 else:
                     # If there is more that one single interface with the min metric, add
-                    # those interfaces as hops at position i in len(sub_path_list)/len(hop)
+                    # those interfaces as hops at position hop_number in len(sub_path_list)/len(hop)
                     num_sub_paths_with_each_interface_in_hop = int(len(sub_path_list)/len(hop))
 
-                    # Each interface will be in len(sub_path_list)/len(hop[i]) sub-paths at hop[i]
+                    # Each interface will be in len(sub_path_list)/len(hop[hop_number]) sub-paths at hop[hop_number]
                     num_interface_appearances = len(hop)
 
                     for sub_path_num in range(num_sub_paths_with_each_interface_in_hop):
                         for interface_appearance in range(num_interface_appearances):
                             print("sub_path_list is {}".format(sub_path_list))
-                            sub_path_list[sub_path_num][i].append(hop[interface_appearance])
+                            sub_path_list[sub_path_num].append(hop[interface_appearance])
 
         normalized_paths.append(sub_path_list)
 
@@ -83,56 +82,72 @@ pprint(a_d)
 normalized_paths = []
 
 for path in a_d['path']:
-
-    # Each path may expand out into multiple paths if the path
-    # has multiple interfaces listed at each hop.
-    # Find the max amount of possible sub-paths
+    print("************** path {} *************".format(path))
     interfaces_in_each_hop = [len(hop) for hop in path]
     max_sub_paths = numpy.prod(interfaces_in_each_hop)
 
     # This is a list of all the created sub-paths
     sub_path_list = []
-    for x in range(max_sub_paths):
+    for sub_path_number in range(max_sub_paths):
         sub_path_list.append([])
 
     # This is the max length of any possible sub-path
-    max_hop = len(path)
+    max_hops = len(path)
 
-    # Each 'hop' is a list of Interfaces from one adjacent node to the
-    # next; there may be multiple Interfaces in that list
-    for i in range(max_hop):
-        print("i is {}".format(i))
-        hop = path[i]
-        print("hop is {}".format(hop))
+    # EXAMPLE
+    # [[Interface(name='A-to-B_2', cost=20, capacity=150, node_object=Node('A'), remote_node_object=Node('B'),
+    #             address='2'),
+    #   Interface(name='A-to-B', cost=20, capacity=125, node_object=Node('A'), remote_node_object=Node('B'),
+    #             address='1')],
 
-        if len(hop) == 1:
-            # There is only one Interface in the hop list; use that interface
-            # Add that single interface to hop i in each sub_path in sub_path_list
-            for j in range(len(sub_path_list)):
-                sub_path_list[j].append(hop)
-        elif len(hop) > 1:
-            # Examine interface cost
-            min_cost = min([interface.cost for interface in hop])
-            hop = [interface for interface in hop if interface.cost == min_cost]
-            if len(hop) == 1:
-                # There is only one Interface in the hop list; use that interface
-                # Add that single interface to hop i in each sub_path in sub_path_list
-                for j in range(len(sub_path_list)):
-                    sub_path_list[j].append(hop[0])
-            else:
-                # If there is more that one single interface with the min metric, add
-                # those interfaces as hops at position i in len(sub_path_list)/len(hop)
-                num_sub_paths_with_each_interface_in_hop = int(len(sub_path_list) / len(hop))
+    # for hop in path:
+    #     for hop_interface in hop:
+    #         if hop.index(hop_interface)%max_sub_paths == 0:
+    #             sub_path_list.index()
 
-                # Each interface will be in len(sub_path_list)/len(hop[i]) sub-paths at hop[i]
-                num_interface_appearances = len(hop)
 
-                for sub_path_num in range(num_sub_paths_with_each_interface_in_hop):
-                    for interface_appearance in range(num_interface_appearances):
-                        print("sub_path_list is {}".format(sub_path_list))
-                        sub_path_list[sub_path_num][i].append(hop[interface_appearance])
+
+    for hop in path:
+        for hop_interface in hop:  # 0, 1, 2
+            hop_interface_index = hop.index(hop_interface)
+
+            for sub_path_num in range(len(sub_path_list)):
+
+
+                print("sub_path = {}".format(sub_path_list[sub_path_number]))
+
+                # TODO - this is the messed up part - finds the first sub path that matches!  not what we want
+                #sub_path_index = sub_path_list.index(sub_path)  # 0, 1, 2, 3, 4, 5
+                # print("({} + {}) % {} = {}".format(hop_interface_index, sub_path_index, len(hop),
+                #                                    ((hop_interface_index + sub_path_index) % len(hop))))
+
+
+
+
+
+
+                print("({} + {}) % {} = {}".format(hop_interface_index, sub_path_num, len(hop),
+                                                   (hop_interface_index + sub_path_num) % len(hop)))
+
+                if (hop_interface_index + sub_path_num) % len(hop) == 0:
+                    # print("hop_interface_index = {}, sub_path_index = {}".format(hop_interface_index, sub_path_index))
+                    # print("hop = {}".format(hop))
+                    # print("hop_interface = {}".format(hop_interface))
+                    # print("sub_path = {}".format(sub_path))
+                    # 0, 0
+                    # 1, 2, 3     1, 2, 3, 4, 5, 6
+
+                    print("appending {} to {}".format(hop_interface, sub_path_list[sub_path_number]))
+                    sub_path_list[sub_path_number].append(hop_interface)
+                    print()
+                    print()
+
 
     normalized_paths.append(sub_path_list)
+
+
+
+
 
 
 
