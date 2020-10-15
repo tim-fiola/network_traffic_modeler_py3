6233492df0779b71cb965497c69b4e2cf01d08e5
tim-fiola <timothy.fiola@gmail.com>
2020-10-14 21:53:03 -0600

    added unit test criteria for manual metric LSPs

--- test/test_rsvp_manual_metric.py
+++ test/test_rsvp_manual_metric.py
@@ -1,1 +1,16 @@
-# TODO - add the unit tests for rsvp manual metric
+# TODO - add the unit tests for rsvp manual metric for both Flex and Performance Models
+
+# 2 parallel LSPs source-dest, but one with a lower than default metric;
+# traffic should only take lower metric LSP
+
+# 1 LSP source-dest, but with higher than default metric;
+# traffic should take that LSP due to better protocol preference;
+# if that LSP fails, IGP routing
+
+# 2 parallel LSPs source-dest, one with more hops than the other;
+# the one with fewer hops has the highest metric; traffic should
+# take the lower-metric LSP
+
+# 2 parallel LSPs source-dest, both with higher than default metric, but
+# one LSP with a higher metric than the other.  Traffic should take lower
+# metric LSP
