06d640c13216051ceab90538056a8b3a1308cfba
tim-fiola <timothy.fiola@gmail.com>
2020-10-29 22:28:39 -0600

    cytoscape_visualization_prototype.py works as intended now.  ready to create a visualation module from it.

--- prototypes/cytoscape_visualization_prototype.py
+++ prototypes/cytoscape_visualization_prototype.py
@@ -4,15 +4,13 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 
-# TODO - need to make callback to show only items selected in dropdown
-# https://dash.plotly.com/layout Core Components section
-
 util_ranges = {'0-24': 'royalblue',
                '25-49': 'green',
                '50-74': 'yellow',
                '75-89': 'orangered',
                '90-99': 'darkred',
-               '100-': 'darkviolet'}
+               '>100': 'darkviolet',
+               'failed': 'grey'}
 
 default_stylesheet = [
     {
@@ -24,6 +22,7 @@ default_stylesheet = [
             'label': "data(label)",
             'line-color': "data(group)",
             "font-size": "6px",
+            "opacity": 0.4
         }
     },
     {
@@ -57,12 +56,6 @@ default_stylesheet = [
 
         }
     },
-    {
-        "selector": 'edge[group=\"util_ranges\", group not in \"util_display_options\"]',
-        "style": {
-            "opacity": 0.4
-        }
-    }
 ]
 
 app = dash.Dash(__name__)
@@ -71,31 +64,31 @@ elements = [
     {'data': {'id': 'one', 'label': 'Node 1'}, 'position': {'x': 75, 'y': 75}},
     {'data': {'id': 'two', 'label': 'Node 2'}, 'position': {'x': 200, 'y': 200}},
     {'data': {'id': 'three', 'label': 'Node 3', 'group': "failed"}, 'position': {'x': 75, 'y': 200}},
-    {'data': {'source': 'one', 'target': 'two', "group": "failed", 'label': 'Ckt4',
+    {'data': {'source': 'one', 'target': 'two', "group": util_ranges["failed"], 'label': 'Ckt4',
               'utilization': 'failed'}},
-    {'data': {'source': 'two', 'target': 'one', "group": "failed", 'label': 'Ckt4',
+    {'data': {'source': 'two', 'target': 'one', "group": util_ranges["failed"], 'label': 'Ckt4',
               'utilization': 'failed'}},
-    {'data': {'source': 'one', 'target': 'three', "group": "failed", 'label': 'Ckt1',
+    {'data': {'source': 'one', 'target': 'three', "group": util_ranges["failed"], 'label': 'Ckt1',
               'utilization': 'failed'}},
-    {'data': {'source': 'three', 'target': 'one', "group": "failed", 'label': 'Ckt1',
+    {'data': {'source': 'three', 'target': 'one', "group": util_ranges["failed"], 'label': 'Ckt1',
               'utilization': 'failed'}},
     {'data': {'source': 'one', 'target': 'three', "group": util_ranges["25-49"], 'label': 'Ckt2',
               'utilization': 40}},
     {'data': {'source': 'three', 'target': 'one', "group": util_ranges["75-89"], 'label': 'Ckt2',
               'utilization': 78}},
-    {'data': {'source': 'one', 'target': 'three', "group": util_ranges["100-"], 'label': 'Ckt3',
+    {'data': {'source': 'one', 'target': 'three', "group": util_ranges[">100"], 'label': 'Ckt3',
               'utilization': 110}},
     {'data': {'source': 'three', 'target': 'one', "group": util_ranges["0-24"], 'label': 'Ckt3',
               'utilization': 15}},
 ]
 
 util_display_options = []
-for item in util_ranges.keys():
-    util_display_options.append({'label': item, 'value': item})
+for util_range, color in util_ranges.items():
+    util_display_options.append({'label': util_range, 'value': color})
 
 app.layout = html.Div([
     cyto.Cytoscape(
-        id='cytoscape-two-nodes',
+        id='cytoscape-prototypes',
         layout={'name': 'preset'},
         style={'width': '100%', 'height': '800px'},
         elements=elements,
@@ -103,7 +96,7 @@ app.layout = html.Div([
     ),
     html.P(id='cytoscape-mouseoverEdgeData-output'),
     html.Label('Multi-Select Dropdown'),
-    dcc.Dropdown(options=util_display_options,
+    dcc.Dropdown(id='utilization-dropdown-callback', options=util_display_options,
                  value=[entry['value'] for entry in util_display_options],
                  multi=True)
 ])
@@ -112,11 +105,27 @@ app.layout = html.Div([
 # Need to select interfaces that have utilization ranges selected in values from dropdown
 
 @app.callback(Output('cytoscape-mouseoverEdgeData-output', 'children'),
-              [Input('cytoscape-two-nodes', 'mouseoverEdgeData')])
+              [Input('cytoscape-prototypes', 'mouseoverEdgeData')])
 def displayTapEdgeData(data):
     if data:
         msg = "Source: {}, Dest: {}, utilization {}%".format(data['source'], data['target'], data['utilization'])
         return msg
+
+
+@app.callback(Output('cytoscape-prototypes', 'stylesheet'), [Input('utilization-dropdown-callback', 'value')])
+def update_stylesheet(edges_to_highlight):
+    new_style = []
+    for color in edges_to_highlight:
+        new_entry = {
+            "selector": "edge[group=\"{}\"]".format(color),
+            "style": {
+                "opacity": 1.0
+            }
+        }
+
+        new_style.append(new_entry)
+
+    return default_stylesheet + new_style
 
 
 if __name__ == '__main__':
