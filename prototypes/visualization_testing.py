cdee6bfd32c45da4c8f66e45952da897a1f44620
tim-fiola <timothy.fiola@gmail.com>
2020-11-01 13:19:22 -0700

    refining visualization_testing.py

--- prototypes/visualization_testing.py
+++ prototypes/visualization_testing.py
@@ -1,23 +1,27 @@
 import sys
 sys.path.append('../')
 
+import dash
+import dash_cytoscape as cyto
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+
 from pprint import pprint
 
 from pyNTM import FlexModel
 
-# Make the Parallel_Link_Model
-model = FlexModel.load_model_file('model_test_topology_multidigraph.csv')
-model.update_simulation()
-print()
+def make_json_node(x, y, id, label, midpoint=False):
+    """
 
-edge_set = set()
-node_set = set()
+    :param x: x-coordinate (or longitude) of node
+    :param y: y-coordinate (or latitude) of node
+    :param id: node identifier within code
+    :param label: Node's displayed label on on layout
+    :param midpoint: Is this a midpoint node?  True|False
 
-import pdb
-pdb.set_trace()
-
-
-def make_json_node(x, y, id, label):
+    :return:
+    """
 
     json_node = {}
     json_node['position'] = {'x': x, 'y': y}
@@ -26,24 +30,19 @@ def make_json_node(x, y, id, label):
     return json_node
 
 
-def make_json_edge(source_id, target_id, label, utilization):
+def make_json_edge(source_id, target_id, label, utilization, util_ranges):
     """
     {'data': {'source': 'two', 'target': 'one', "group": util_ranges["failed"], 'label': 'Ckt4',
               'utilization': 'failed'}}
 
     """
 
-    util_ranges = {'0-24': 'royalblue',
-                   '25-49': 'green',
-                   '50-74': 'yellow',
-                   '75-89': 'orangered',
-                   '90-99': 'darkred',
-                   '>100': 'darkviolet',
-                   'failed': 'grey'}
     group = None
 
-    if utilization.lower() == 'int is down':
-        group == 'failed'
+    if utilization == 'Int is down':
+        group = 'failed'
+    elif utilization >= 100:
+        group = util_ranges['100+']
     else:
         for range in util_ranges.keys():
             try:
@@ -63,13 +62,23 @@ def make_json_edge(source_id, target_id, label, utilization):
 
     return json_edge
 
-# util_ranges = {'0-24': 'royalblue',
-#                '25-49': 'green',
-#                '50-74': 'yellow',
-#                '75-89': 'orangered',
-#                '90-99': 'darkred',
-#                '>100': 'darkviolet',
-#                'failed': 'grey'}
+
+util_ranges = {'0-24': 'royalblue',
+               '25-49': 'green',
+               '50-74': 'yellow',
+               '75-89': 'orangered',
+               '90-99': 'darkred',
+               '100+': 'darkviolet',
+               'failed': 'grey'}
+
+# Make the Parallel_Link_Model
+model = FlexModel.load_model_file('model_test_topology_multidigraph.csv')
+model.update_simulation()
+print()
+
+edges = []
+nodes = []
+
 
 for ckt in model.circuit_objects:
     int_a = ckt.interface_a
@@ -80,24 +89,125 @@ for ckt in model.circuit_objects:
     node_a_x = node_a.lon
     node_b_y = node_b.lat
     node_b_x = node_b.lon
-    ckt_id = int_a.circuit_id
 
-    midpoint_x = sum(node_a_x, node_b_x)/2
-    midpoint_y = sum(node_a_y, node_b_y)/2
+    try:
+        ckt_id = int_a.circuit_id
+    except (AttributeError, ValueError):
+        ckt_id = "circuit_{}-{}".format(node_a.name, node_b.name)
+
+    midpoint_x = sum([node_a_x, node_b_x])/2
+    midpoint_y = sum([node_a_y, node_b_y])/2
 
     # Create the midpoint node between the endpoints
-    midpoint_label = 'midpoint-{}'.format(ckt_id)
-    node_set.add(make_json_node(midpoint_x, midpoint_y, midpoint_label, midpoint_label))
+    # midpoint_label = 'midpoint-{}'.format(ckt_id)
+    midpoint_label = 'midpoint-{}-{}'.format(node_a.name, node_b.name)
+    new_node = make_json_node(midpoint_x, midpoint_y, midpoint_label, midpoint_label)
+    nodes.append(new_node)
     # Create each end node
-    node_set.add(make_json_node(node_a.lon, node_a.lat, node_a.name, node_a.name))
-    node_set.add(make_json_node(node_b.lon, node_b.lat, node_b.name, node_b.name))
+    nodes.append(make_json_node(node_a.lon, node_a.lat, node_a.name, node_a.name))
+    nodes.append(make_json_node(node_b.lon, node_b.lat, node_b.name, node_b.name))
 
     # Create the edges
     # {'data': {'source': 'two', 'target': 'one', "group": util_ranges["failed"], 'label': 'Ckt4',
     #           'utilization': 'failed'}}
 
     # Make edges with midpoints
-    edge_set.add(make_json_edge(node_a.name, midpoint_label, ckt_id, int_a.utilization))
-    edge_set.add(make_json_edge(node_b.name, midpoint_label, ckt_id, int_b.utilization))
+    edges.append(make_json_edge(node_a.name, midpoint_label, ckt_id, int_a.utilization, util_ranges))
+    edges.append(make_json_edge(node_b.name, midpoint_label, ckt_id, int_b.utilization, util_ranges))
+
+default_stylesheet = [
+    {
+        "selector": 'edge',
+        "style": {
+            "mid-target-arrow-color": "blue",
+            "mid-target-arrow-shape": "vee",
+            "curve-style": "bezier",
+            'label': "data(label)",
+            'line-color': "data(group)",
+            "font-size": "8px",
+            "opacity": 0.4
+        }
+    },
+    {
+        "selector": "edge[group=\"failed\"]",
+        "style": {
+            "line-color": "#808080",
+            "curve-style": "bezier",
+            'label': "data(label)",
+            "font-size": "8px",
+            'line-style': 'dashed'
+        }
+    },
+    {
+        "selector": "node",
+        "style": {
+            "label": "data(label)",
+            "font-size": "8px",
+            "text-halign": 'center',
+            'text-valign': 'center',
+            'text-wrap': 'wrap',
+            "size": "10px"
+        }
+    },
+    {
+        "selector": 'node[group=\"failed\"]',
+        "style": {
+            'text-color': '#FF0000',
+            'shape': 'rectangle',
+            'label': "data(label)",
+            'background-color': 'red'
+
+        }
+    },
+]
+
+elements = nodes + edges
+
+util_display_options = []
+for util_range, color in util_ranges.items():
+    util_display_options.append({'label': util_range, 'value': color})
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    cyto.Cytoscape(
+        id='cytoscape-prototypes',
+        layout={'name': 'preset'},
+        style={'width': '100%', 'height': '800px'},
+        elements=elements,
+        stylesheet=default_stylesheet,
+    ),
+    html.P(id='cytoscape-mouseoverEdgeData-output'),
+    html.Label('Multi-Select Dropdown'),
+    dcc.Dropdown(id='utilization-dropdown-callback', options=util_display_options,
+                 value=[entry['value'] for entry in util_display_options],
+                 multi=True)
+])
 
 
+# Need to select interfaces that have utilization ranges selected in values from dropdown
+@app.callback(Output('cytoscape-mouseoverEdgeData-output', 'children'),
+              [Input('cytoscape-prototypes', 'mouseoverEdgeData')])
+def displayTapEdgeData(data):
+    if data:
+        msg = "Source: {}, Dest: {}, utilization {}%".format(data['source'], data['target'], data['utilization'])
+        return msg
+
+
+@app.callback(Output('cytoscape-prototypes', 'stylesheet'), [Input('utilization-dropdown-callback', 'value')])
+def update_stylesheet(edges_to_highlight):
+    new_style = []
+    for color in edges_to_highlight:
+        new_entry = {
+            "selector": "edge[group=\"{}\"]".format(color),
+            "style": {
+                "opacity": 1.0
+            }
+        }
+
+        new_style.append(new_entry)
+
+    return default_stylesheet + new_style
+
+
+app.run_server(debug=True)
