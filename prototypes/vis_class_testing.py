7dcb49c0ac6becb07fff87085630580065c93604
tim-fiola <timothy.fiola@gmail.com>
2020-12-15 22:05:03 -0700

    tried to make weathermap_class.py work, could not.  visualization_testing_3.py is the best candidate now; work on that

--- prototypes/vis_class_testing.py
+++ prototypes/vis_class_testing.py
@@ -4,6 +4,7 @@ sys.path.append('../')
 from pyNTM import FlexModel
 from visualization_testing_3 import make_visualization
 from pyNTM import visualization_utility as vu
+from pyNTM.weathermap_class import Weathermap
 #
 #
 #
@@ -11,7 +12,10 @@ model = FlexModel.load_model_file('igp_shortcuts_model.csv')
 model.update_simulation()
 
 # vis = vu.make_visualization(model)
-make_visualization(model)
+# make_visualization(model)
+
+wm = Weathermap(model)
+wm.create_visualization()
 
 
 
