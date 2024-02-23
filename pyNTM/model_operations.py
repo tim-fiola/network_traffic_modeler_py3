34c3bff967a16abd4b1889446a9c97b25be06757
tim-fiola <timothy.fiola@gmail.com>
2024-02-22 20:45:45 -0700

    note to self on model_operations.py for what to add

--- pyNTM/model_operations.py
+++ pyNTM/model_operations.py
@@ -1,6 +1,24 @@
 
+## Focus on items where uniqueness is important (interfaces _key, node_name, lsp _key, look at the model for uniqueness checks)
+#  Add Circuit (2 interfaces)
 
-#  Add Interface
+
+# Add LSP
 
 
-#
+# Remove Circuit
+
+
+# Remove LSP
+
+
+# Add demand
+
+
+# Delete demand
+
+
+
+
+
+
